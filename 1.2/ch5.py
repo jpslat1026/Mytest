e8e4d5bd5b500fdb5843692e63a35d7a4ca853af
jpslat1026 <45008278+jpslat1026@users.noreply.github.com>
2018-11-14 16:11:15 -0500

    Create ch5.py

---
+++ 1.2/ch5.py
@@ -0,0 +1,5 @@
+def main() :
+    print("This script will calculate how long it takes a photo from Curiosity to Reach NASA at earths cloest distance")
+    print("Earth's cloest distance is 34,000,000")
+    
+    print("the phoo travels at 186,000 miles per second")
