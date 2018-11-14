1edffe0ad876a6a9b74e6716d6cb05e66ed42137
jpslat1026 <21slatjo@wtahs.com>
2018-11-14 13:51:36 -0500

    Reorganized

---
+++ 1.2/ch4.py
@@ -0,0 +1,11 @@
+
+def main():
+   print("This program computes the average of three exam scores. ")
+
+   1 = input("What is the player’s name? ")
+   hit = input("How many hits? ")
+   bat = input("How many at-bats? ")
+   average = (hit + hit) / 2
+
+   print(1, "batting average is", average)
+main()
