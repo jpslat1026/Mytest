7e511375a2d8a85ef3d92a0ba0da523ada487a73
jpslat1026 <21slatjo@wtahs.com>
2018-12-12 11:10:53 -0500

    updated

--- Section 1/1.4/7.py
+++ Section 1/1.4/7.py
@@ -2,6 +2,7 @@
 #by John Slattery on November 19, 2018
 #This script wil make a circle for a given radius
 
+import math
 import graphics
 from graphics import *
 import time
@@ -11,10 +12,14 @@ def main():
     win = GraphWin("Circle", 300, 300)
     Text(Point(75, 200), " Radius: "). draw(win)
     r = Entry(Point(200, 200), 10).draw(win)
+    Text(Point(75, 100), " Y intercept: "). draw(win)
+    yint = Entry(Point(200,100), 10).draw(win)
     Text(Point(100, 10), "Click to exit").draw(win)
     win.getMouse()
     r = r.getText()
     r = eval(r)
+    yint = yint.getText()
+    yint = eval(yint)
     
     #other
     win = GraphWin("cir", 500,500)
@@ -26,16 +31,27 @@ def main():
 
     
     #line
-    l = Line(Point(0,0), Point(r,0))
+    l = Line(Point(-10,yint), Point(10,yint))
+
     l.draw(win)
 
+    
+
+    #Y intercept
+    x = math.sqrt(abs(((r ** 2) - (yint **2))))
+    print(x)
+    cir = Circle(Point(x,yint), 0.5)
+    cir.setFill("red")
+    cir.draw(win)
+    cir = Circle(Point(-x,yint), 0.5)
+    cir.setFill("red")
+    cir.draw(win)
+    x = str(x)
     #text raduis = ?
-    r = str(r)
-    t = Text(Point(0.020040080160320883, 0.4208416833667332), "r = " + r)
+    t = Text(Point(0.020040080160320883, 0.4208416833667332), "The X intercept is " + x + " and -" + x)
     t.draw(win)
 
     #close
-    a = win.getMouse()
-    print(a)
+    win.getMouse()
     win.close()
 main()
---
+++ Section2/2.1/1.py
@@ -0,0 +1,16 @@
+s1 = "spam"
+s2 = "ni!"
+#print("The Knights who say, " + s2)
+#print(3 * s1 + 2 * s2)
+#print(s1[1])
+#print(s1[1:3])
+#print(s1[2] + s2[:2])
+#print(s1 + s2[-1])
+#print(s1.upper())
+#print(s2.upper().ljust(4) * 3)
+#print(s2.upper())
+#print(s2 + s1 + s2)
+#print(s1+s2+s1+s2+s1+s2)
+#print(s1[0:2] + s1[3])
+for ch in "aardvark":
+    print(ch)
