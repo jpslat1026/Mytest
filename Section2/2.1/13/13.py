1c752a956d66454e88faba922afd36d60eb5deda
jpslat1026 <21slatjo@wtahs.com>
2018-12-14 08:27:45 -0500

    updated

--- Section2/2.1/13/13.py
+++ Section2/2.1/13/13.py
@@ -14,14 +14,15 @@ def main():
             infileedName = input("where is the file you would like to decode? ")
             infilee = open(infileedName, "r")
             infilee = infilee.read()
-            x = input("what is the key(ex: 7,8): ")
+            x = input("what is the key(ex: 7,8,2): ")
             x.split(",")
             y = eval(x[2])
+            z = eval(x[3])
             x = eval(x[0])
             b = 0
             c = ""
             for l in infilee:
-                a = ord(l) + x - x + y
+                a = ord(l) + x - z + y
                 c = chr(a) + c
             print(c)
             s = input("Would you like output your file(y/n): ")
@@ -37,14 +38,15 @@ def main():
             infiledName = input("where is the file you would like to decode? ")
             infiled = open(infiledName, "r")
             infiled = infiled.read()
-            x = input("what is the key(ex: 7,8): ")
+            x = input("what is the key(ex: 7,8,2): ")
             x.split(",")
             y = eval(x[2])
+            z = eval(x[3])
             x = eval(x[0])
             b = 0
             c = ""
             for l in infiled:
-                a = ord(l) - x + x - y
+                a = ord(l) - x + z - y
                 c = chr(a) + c
             print(c)
             s = input("Would you like output your file(y/n): ")
--- Section2/2.1/8.py
+++ Section2/2.1/8.py
@@ -10,27 +10,29 @@ def main():
         loose = input("would you like to encrypte(e) or decrypte(d): ")
         if loose == "e":
             message = input("Enter your Message to encrypte: ")
-            x = input("what is the key(ex: 7,8): ")
+            x = input("what is the key(ex: 7,8,4): ")
             x.split(",")
             y = eval(x[2])
+            z = eval(x[3])
             x = eval(x[0])
             b = 0
             c = ""
             for l in message:
-                a = ord(l) + x - x + y
+                a = ord(l) + x - z + y
                 c = chr(a) + c
             print(c)
             x = input("Would you like to do it again(y/n): ")
         if loose == "d":
             message = input("Enter your Message to decrypt: ")
-            x = input("what is the key(ex: 7,8): ")
+            x = input("what is the key(ex: 7,8,4): ")
             x.split(",")
             y = eval(x[2])
+            z = eval(x[3])
             x = eval(x[0])
             b = 0
             c = ""
             for l in message:
-                a = ord(l) - x + x - y
+                a = ord(l) - x + z - y
                 c = chr(a) + c
             print(c)
             x = input("Would you like to do it again(y/n): ")
